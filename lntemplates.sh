cfa515d322da4e091a32f7e883493e39eee61b5f
iTBoyer <itboyer@qq.com>
2019-12-02 13:49:53 +0800

    upate

--- lntemplates.sh
+++ lntemplates.sh
@@ -6,14 +6,16 @@
 #  Created by pengyucheng on 28/04/2017.
 #  Copyright © 2017 hexo主题. All rights reserved.
 
-SRC_HOME=`pwd`
+basepath=$(cd `dirname $0`; pwd)
+echo "当前cd的目录名："`basename $(pwd)`
+echo "sh脚本文件的绝对路径：$basepath"
 
 XcodePATH="~/Library/Developer/Xcode/Templates/File\ Templates/HexoMD"
 echo "mkdir -p ${XcodePATH}"
 mkdir -p ~/Library/Developer/Xcode/Templates/File\ Templates/HexoMD
 mkdir -p ~/Library/Developer/Xcode/Templates/Project\ Templates/ProjectTemplates
 
-echo "ln -fs ${SRC_HOME}/Util/HexoTemplates ${XcodePATH}"
-ln -fs ${SRC_HOME}/Util/HexoTemplates ~/Library/Developer/Xcode/Templates/File\ Templates/HexoMD
-ln -fs ${SRC_HOME}/Util/HexoTemplates/ProjectTemplates ~/Library/Developer/Xcode/Templates/Project\ Templates/ProjectTemplates
+echo "ln -fs $basepath/HexoTemplates ${XcodePATH}"
+ln -fs $basepath/HexoTemplates ~/Library/Developer/Xcode/Templates/File\ Templates/HexoMD
+ln -fs $basepath/HexoTemplates/ProjectTemplates ~/Library/Developer/Xcode/Templates/Project\ Templates/ProjectTemplates
 echo "安装hexo模版完成"
